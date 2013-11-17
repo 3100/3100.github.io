2469b7202b2130b9b61cfb9e1c2fbbc7b3ce09fd
3100 <sugar16g@gmail.com>
2013-11-17 14:05:35 +0900

    Use fontawesome and improve some layouts

--- source/_partials/asides/_recent_articles.rb
+++
@@ -1,8 +0,0 @@
-article#recent_articles
-  h3 Recent Articles
-  ul
-    - blog.articles[0...5].each do |article|
-      li
-        = link_to article.title, article
-        = partial "date", locals: {date: article.date}
-
